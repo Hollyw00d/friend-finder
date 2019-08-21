69ce389bef0355d9e613a8d769020b5b6b21a82c
Matt Jennings <matt@mattjennings.net>
2019-08-20 19:48:53 -0700

    added pseudo coding in readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# friend-finder
-Friend Finder Node and Express Project
+# Friend Finder
+## Friend Finder Node and Express Project
+### Pseudo Coding
+1. Work on Pseudo code for friends score comparison.
+
+2. Do math in API post route (apiRoutes.js) in post route.
+
+3. From front end do $.post and $.ajax from front end.
+
+4. Have a variable called "bestMatch" and write it as an object.
+
+5. Need access to users' scores and data.
+
+6. Use map loop to go over all friends in JSON object. And do another map loop inside to compare scores.
+
+7. Use similar code as below:
+````if (totalDifference <= bestMatch.friendDifference) {   
+````// Reset the bestMatch to be the new friend.  
+````bestMatch.name = currentFriend.name;  
+````bestMatch.photo = currentFriend.photo;  
+````bestMatch.friendDifference = totalDifference;  
+ ````}  
---
+++ app/data/friends.js
@@ -0,0 +1,708 @@
+var friends = [
+
+        {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+        },
+        {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+        ]
+        },
+        {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+        ]
+        },
+        {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+        ]
+        },
+        {
+        "name": "James hawthorne",
+        "photo": "https://previews.123rf.com/images/racorn/racorn1308/racorn130805649/21341221-profile-portrait-of-a-charming-young-business-woman-being-happy-and-smiling-in-an-office-setting-.jpg",
+        "scores": [
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "jessica",
+        "photo": "https://m.media-amazon.com/images/M/MV5BNjk2ODQzNDYxNV5BMl5BanBnXkFtZTgwMTcyNDg4NjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        "scores": [
+        "2",
+        "2",
+        "4",
+        "3",
+        "3",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4"
+        ]
+        },
+        {
+        "name": "adfasdfasdf",
+        "photo": "fsadfsdf",
+        "scores": [
+        "2",
+        "2",
+        "2",
+        "2",
+        "5",
+        "3",
+        "3",
+        "2",
+        "3",
+        "3"
+        ]
+        },
+        {
+        "name": "s",
+        "photo": "s",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "s",
+        "photo": "s",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "s",
+        "photo": "s",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "s",
+        "photo": "s",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "s",
+        "photo": "s",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "asdasd",
+        "photo": "asdasd",
+        "scores": [
+        "3",
+        "2",
+        "3",
+        "3",
+        "3",
+        "4",
+        "3",
+        "4",
+        "2",
+        "2"
+        ]
+        },
+        {
+        "name": "asdasd",
+        "photo": "asdasd",
+        "scores": [
+        "3",
+        "2",
+        "3",
+        "3",
+        "3",
+        "4",
+        "3",
+        "4",
+        "2",
+        "2"
+        ]
+        },
+        {
+        "name": "s",
+        "photo": "s",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Steve",
+        "photo": "https://picsum.photos/id/237/200/300",
+        "scores": [
+        "2",
+        "4",
+        "2",
+        "1",
+        "2",
+        "2",
+        "4",
+        "3",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "fart",
+        "photo": "fart",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "fart",
+        "photo": "fart",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "asd",
+        "photo": "asdfasdfasdf",
+        "scores": [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "Johnny Blaze",
+        "photo": "https://cdn.mcuexchange.com/wp-content/uploads/2017/05/ghost-rider-featured.jpg",
+        "scores": [
+        "5",
+        "1",
+        "5",
+        "3",
+        "5",
+        "3",
+        "1",
+        "1",
+        "5",
+        "3"
+        ]
+        },
+        {
+        "name": "Johnny Blaze",
+        "photo": "https://cdn.mcuexchange.com/wp-content/uploads/2017/05/ghost-rider-featured.jpg",
+        "scores": [
+        "5",
+        "1",
+        "5",
+        "3",
+        "5",
+        "3",
+        "1",
+        "1",
+        "5",
+        "3"
+        ]
+        },
+        {
+        "name": "asd",
+        "photo": "asdfasdfasdf",
+        "scores": [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "asd",
+        "photo": "asdfasdfasdf",
+        "scores": [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "Jane Smith",
+        "photo": "https://www.pexels.com/photo/woman-taking-selfie-1288171/",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "3",
+        "5",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3"
+        ]
+        },
+        {
+        "name": "dw",
+        "photo": "asd",
+        "scores": [
+        "2",
+        "3",
+        "1",
+        "3",
+        "3",
+        "2",
+        "5",
+        "2",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "dw",
+        "photo": "asd",
+        "scores": [
+        "2",
+        "3",
+        "1",
+        "3",
+        "3",
+        "2",
+        "5",
+        "2",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "dw",
+        "photo": "asd",
+        "scores": [
+        "2",
+        "3",
+        "1",
+        "3",
+        "3",
+        "2",
+        "5",
+        "2",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "sx",
+        "photo": "sx",
+        "scores": [
+        "1",
+        "3",
+        "5",
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "2",
+        "2"
+        ]
+        },
+        {
+        "name": "sx",
+        "photo": "sx",
+        "scores": [
+        "1",
+        "3",
+        "5",
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "2",
+        "2"
+        ]
+        },
+        {
+        "name": "Asia O",
+        "photo": "google.com/imghp",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "4",
+        "4",
+        "4",
+        "4",
+        "3",
+        "3",
+        "3"
+        ]
+        },
+        {
+        "name": "Chaitra",
+        "photo": "gh",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Chaitra",
+        "photo": "gh",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Chaitra",
+        "photo": "gh",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Chaitra",
+        "photo": "gh",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Chaitra",
+        "photo": "gh",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "COti",
+        "photo": "https://friend-finder-fsf.herokuapp.com/survey",
+        "scores": [
+        "3",
+        "3",
+        "1",
+        "5",
+        "4",
+        "3",
+        "4",
+        "4",
+        "4",
+        "3"
+        ]
+        },
+        {
+        "name": "Name",
+        "photo": "NaN",
+        "scores": [
+        "4",
+        "3",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "3",
+        "1"
+        ]
+        },
+        {
+        "name": "asd",
+        "photo": "asd",
+        "scores": [
+        "3",
+        "3",
+        "4",
+        "3",
+        "3",
+        "3",
+        "2",
+        "3",
+        "3",
+        "3"
+        ]
+        },
+        {
+        "name": "MJ",
+        "photo": "https://media.licdn.com/dms/image/C5603AQHfsHyeUQlB3Q/profile-displayphoto-shrink_200_200/0?e=1571875200&v=beta&t=b4zUrKnYd_ttnEzlCPp2AIlhQsI8yhFTLJx_-xMFNvc",
+        "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "asdf",
+        "photo": "adfasdf",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "asdf",
+        "photo": "adfasdf",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "asdf",
+        "photo": "adfasdf",
+        "scores": [
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        }
+
+];
